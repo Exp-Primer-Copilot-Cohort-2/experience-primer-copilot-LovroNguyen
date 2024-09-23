c68bba80cbc1d9728e29b5115bea164d568dcac0
Nguyen Nhan <86762968+LovroNguyen@users.noreply.github.com>
2024-09-23 13:09:50 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,16 @@
+function skillsMember() {
+  return [
+    { name: 'html', level: 6 },
+    { name: 'css', level: 6 },
+    { name: 'javascript', level: 5 },
+    { name: 'react', level: 4 },
+    { name: 'nodejs', level: 3 },
+    { name: 'mongodb', level: 3 },
+    { name: 'python', level: 3 },
+    { name: 'ruby', level: 2 },
+    { name: 'csharp', level: 2 },
+    { name: 'java', level: 2 },
+    { name: 'php', level: 2 },
+    { name: 'sql', level: 2 }
+  ]
+}
